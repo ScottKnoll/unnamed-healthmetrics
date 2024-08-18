13c501fc98ab138d6bce086813b541a9366c1ecf
Scott Knoll <scknoll88@gmail.com>
2024-08-17 20:06:54 -0500

    goals create

--- resources/views/goals/create.blade.php
+++ resources/views/goals/create.blade.php
@@ -11,7 +11,6 @@
             </div>
         </div>
     </x-slot>
-
     <x-container>
         <div class="lg:grid lg:grid-cols-12 lg:gap-x-5">
             <aside class="px-2 py-6 sm:px-6 lg:col-span-3 lg:px-0 lg:py-0">
@@ -63,7 +62,6 @@
                     </a>
                 </nav>
             </aside>
-
             <div class="space-y-6 sm:px-6 lg:col-span-9 lg:px-0">
                 <form action="/goals" method="POST">
                     @csrf
@@ -75,88 +73,77 @@
                                     friendships, participating in social activities, or building a social support
                                     network.</p>
                             </div>
-
                             <div class="grid grid-cols-3 gap-6">
                                 <div class="col-span-3">
-                                    <x-label for=>My
-                                        5-year Goal:</x-label>
-                                    <div class="mt-2">
-                                        <x-textarea id="five_year" name="five_year" :value="old('five_year]')"
-                                            rows="3"></x-textarea>
-                                    </div>
+                                    <x-label class="mb-2" for=>My 5-year Goal:</x-label>
+                                    <x-textarea id="five_year" name="five_year" :value="old('five_year]')"
+                                        rows="3"></x-textarea>
+                                    <x-input-error :messages="$errors->get('five_year')" class="mt-2" />
                                 </div>
-
                                 <div class="col-span-3">
-                                    <x-label for>My
-                                        1-year Goal:</x-label>
-                                    <div class="mt-2">
-                                        <x-textarea id="one_year" name="one_year" :value="old('one_year')"
-                                            rows="3"></x-textarea>
-                                    </div>
+                                    <x-label class="mb-2" for>My 1-year Goal:</x-label>
+                                    <x-textarea id="one_year" name="one_year" :value="old('one_year')"
+                                        rows="3"></x-textarea>
+                                    <x-input-error :messages="$errors->get('one_year')" class="mt-2" />
                                 </div>
-
                                 <div class="col-span-3">
-                                    <x-label for=>My
-                                        1-month Goal:</x-label>
-                                    <div class="mt-2">
-                                        <x-textarea id="one_month" name="one_month" :value="old('one_month')"
-                                            rows="3"></x-textarea>
-                                    </div>
+                                    <x-label class="mb-2" for=>My 1-month Goal:</x-label>
+                                    <x-textarea id="one_month" name="one_month" :value="old('one_month')"
+                                        rows="3"></x-textarea>
+                                    <x-input-error :messages="$errors->get('one_month')" class="mt-2" />
                                 </div>
                             </div>
                         </div>
-
                         <div class="px-4 py-6 space-y-6 bg-white sm:p-6">
                             <div>
                                 <h3 class="text-base font-semibold leading-6 text-gray-900">S.M.A.R.T. Goals</h3>
                                 <p class="mt-1 text-sm text-gray-500">Write goals that are measurable.</p>
                             </div>
-
                             <div class="grid grid-cols-1 gap-6">
                                 <div>
-                                    <x-label for="smart_goals[specific]">Specific</x-label>
+                                    <x-label class="mb-2" for="smart_goals[specific]">Specific</x-label>
                                     <x-textarea id="smart_goals[specific]" name="smart_goals[specific]"
                                         :value="old('smart_goals[specific]')" rows="3"></x-textarea>
                                     <p class="mt-2 text-sm text-gray-500">What do you want to accomplish? Who needs to
                                         be included? When do you want to do this? Why is this a goal?</p>
+                                    <x-input-error :messages="$errors->get('smart_goals.specific')" class="mt-2" />
                                 </div>
-
                                 <div>
-                                    <x-label for="smart_goals[measurable]">Measurable</x-label>
+                                    <x-label class="mb-2" for="smart_goals[measurable]">Measurable</x-label>
                                     <x-textarea id="smart_goals[measurable]" name="smart_goals[measurable]"
                                         :value="old('smart_goals[measureable]')" rows="3"></x-textarea>
                                     <p class="mt-2 text-sm text-gray-500">How can you measure progress and know if
                                         you’ve successfully met your goal?</p>
+                                    <x-input-error :messages="$errors->get('smart_goals.measurable')" class="mt-2" />
                                 </div>
-
                                 <div>
-                                    <x-label for="smart_goals[achievable]">Achievable</x-label>
+                                    <x-label class="mb-2" for="smart_goals[achievable]">Achievable</x-label>
                                     <x-textarea id="smart_goals[achievable]" name="smart_goals[achievable]"
                                         :value="old('smart_goals[achievable]')" rows="3"></x-textarea>
                                     <p class="mt-2 text-sm text-gray-500">Do you have the skills required to achieve
                                         the goal? If not, can you obtain them? What is the motivation for this goal? Is
                                         the amount of effort required on par with what the goal will achieve?</p>
+                                    <x-input-error :messages="$errors->get('smart_goals.achievable')" class="mt-2" />
                                 </div>
-
                                 <div>
-                                    <x-label for="smart_goals[relevant]">Relevant</x-label>
+                                    <x-label class="mb-2" for="smart_goals[relevant]">Relevant</x-label>
                                     <x-textarea id="smart_goals[relevant]" name="smart_goals[relevant]"
                                         :value="old('smart_goals[relevant]')" rows="3"></x-textarea>
                                     <p class="mt-2 text-sm text-gray-500">Why am I setting this goal now? Is it aligned
                                         with overall objectives?</p>
+                                    <x-input-error :messages="$errors->get('smart_goals.relevant')" class="mt-2" />
                                 </div>
-
                                 <div>
-                                    <x-label for="smart_goals[time-bound]">Time-bound</x-label>
+                                    <x-label class="mb-2" for="smart_goals[time-bound]">Time-bound</x-label>
                                     <x-textarea id="smart_goals[time-bound]" name="smart_goals[time-bound]"
                                         :value="old('smart_goals[time-bound]')" rows="3"></x-textarea>
                                     <p class="mt-2 text-sm text-gray-500">What’s the deadline and is it realistic?</p>
+                                    <x-input-error :messages="$errors->get('smart_goals.time-bound')" class="mt-2" />
                                 </div>
                             </div>
                         </div>
                         <div class="px-4 py-3 text-right bg-gray-50 sm:px-6">
-                            <button type="submit"
-                                class="inline-flex justify-center px-3 py-2 text-sm font-semibold text-white bg-indigo-600 rounded-md shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+                            <x-button type="submit" styles="indigo">Save</x-button>
                         </div>
                     </div>
                 </form>
